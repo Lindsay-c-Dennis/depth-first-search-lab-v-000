a5dab7491938073ce7190085d427a4f2a5fdd400
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-07-13 17:03:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,23 @@ function depthFirstSearch(rootNode, vertices, edges) {
   }
 }
 
-function findAdjacent(rootNodeName, vertices, edges) {
-  
+function findAdjacent(rootNode, vertices, edges) {
+  let links = edges.filter(edge => edge.includes(rootNode));
+  links = [].concat.apply([], links);
+  let nodeNames = links.filter(p => p !== rootNode);
+  let adjNodes = [];
+  for (let i=0; i<nodeNames.length; i++) {
+    let node = findNode(nodeNames[i], vertices);
+    adjNodes.push(node);
+  }
+  return adjNodes.filter(node => node.distance === null);
+}
+
+function findNode(nodeName, vertices) {
+  for (let i=0; i<vertices.length; i++) {
+    if (vertices[i].name === nodeName) {
+      return vertices[i];
+    }
+  }
+
 }
