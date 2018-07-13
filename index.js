7d491c8f18b8afcdecddf352bf2b7d4c23902183
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-07-13 17:08:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ function depthFirstSearch(rootNode, vertices, edges) {
   let stack = [rootNode];
   let discovered = [rootNode];
   while (stack.length !== 0) {
-    let currentNode = stack[0];
+    let currentNode = stack.pop();
     if (currentNode.discovered === null) {
       currentNode.discovered = true;
-      discovered = discovered.concat()
+      let adjNodes = findAdjacent(currentNode.name, vertices, edges);
+      stack = stack.concat(adjNodes);
+      discovered.push(currentNode);
     }
   }
+  return discovered;
 }
 
 function findAdjacent(rootNode, vertices, edges) {
