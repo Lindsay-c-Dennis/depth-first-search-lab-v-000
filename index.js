3a8bb43275148ff142f5c9e150040c4b2c98f641
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-07-13 16:57:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function depthFirstSearch(rootNode, vertices, edges) {
   let stack = [rootNode];
+  let discovered = [rootNode]
   while (stack.length !== 0) {
     let currentNode = stack[0];
     
